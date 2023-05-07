7d0831ef4d649aeba393b726d21770a6333ffd70
Azizova Irina <i_r_i_n_a_nikitina@mail.ru>
2023-05-07 20:17:06 +0300

    feat: add list function

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const fs = require('fs');
+const {constans} = fs;
 const {argv} = process;
 
-function init() {
-    fs.readFile(path.join())
+/* function init() {
+    fs.access('notes.json', fs.F_OK, (err) => {
+        if (err) {          
+            fs.writeFile('notes.json', '[]', err => {
+                if (err) throw err;
+                console.log('Файл notes.json создан');
+            });
+        }
+    });    
 }
+init(); */
 
 const [command, title, content] = argv.slice(2);
 
@@ -11,10 +20,15 @@ switch (command) {
     case 'create':
         create(title, content);
         break;
+    case 'list': 
+        list();
+        break;
     default: console.log('Неизвестная команда.');
 }
 
-function create(title, content) {
+
+function create(title, content) {    
+
     fs.readFile('notes.json', (err, data) => {
         if (err) throw err;
         const notes = JSON.parse(data);
@@ -24,6 +38,18 @@ function create(title, content) {
         fs.writeFile('notes.json', json, err => {
             if (err) throw err;
             console.log('Заметка создана');
+        });
+    });
+    console.log('create');
+}
+
+
+function list() {
+    fs.readFile('notes.json', (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        notes.forEach((note, i) => {
+            console.log(`${i + 1} - ${note.title} - ${note.content}`);
         });
     });
 }
